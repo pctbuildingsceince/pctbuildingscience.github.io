437e1a317e18a284eab33156b8a8f8a4c0fa5cc9
LTimusk <lukas.timusk@ucc.on.ca>
2022-06-25 19:19:50 -0400

    Captialize?

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "lukas.timusk@ucc.on.ca";
+    $email_to = "timusk@hotmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
